6da958ad5e7047caa548621cbf484adfd3641131
Lemonsity <Lemonsity@lemon.com>
2019-04-16 14:10:33 -0400

    Adding calculation; Motify Variables

---
+++ Lesson-2/Calculation.py
@@ -0,0 +1,25 @@
+a = 3
+b = 5
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(int(a / b))
+print(a % b)
+
+#string1 = 1 + "String"
+string2 = 2 * "String"
+#string3 = 3 - "String"
+#string4 = 4 / "String"
+
+boolean1 = True # True = 1, False = 0
+boolean2 = not boolean1
+
+boolean3 = True and True
+boolean4 = True and False
+boolean5 = False and False
+
+boolean6 = True or True
+boolean7 = True or False
+boolean8 = False or False
