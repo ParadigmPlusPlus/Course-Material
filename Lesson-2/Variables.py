70bf5b8d039e23bec19858d0c232c19c48ae55f4
Lemonsity <youzhang.mark.sun@gmail.com>
2019-04-15 20:41:50 -0400

    Adding course material

---
+++ Lesson-2/Variables.py
@@ -0,0 +1,31 @@
+int1 = 1;
+int2 = 123456789
+int3 = 10
+int4 = 0b10
+int5 = 0o10
+int6 = 0x10
+
+float1 = 2.3
+float2 = .4e7
+float3 = .4e-4
+
+bigFloat = 1.79e308
+infiniteFloat = 1.8e308
+
+smallFloat = 5e-324
+zeroFloat = 1e-325
+
+complex1 = 1 + 1j
+
+string1 = 'String'
+string2 = "String"
+string3 = ''
+
+
+string4 = "It's a me, Mario!"
+string5 = "It's a me\nMario"
+string6 = '''"It's a me", said Mario'''
+
+boolean1 = True
+boolean2 = False
+
