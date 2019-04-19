7c3992a48da0c8c6435a66f845038f4b2f1d30cd
Lemonsity <youzhang.mark.sun@gmail.com>
2019-04-18 21:22:58 -0400

    Adding lesson

--- Lesson-4/LoopsWSelection.py
+++ Lesson-4/LoopsWSelection.py
@@ -4,3 +4,17 @@ for i in range (10):
         print("even");
     else:
         print("odd");
+        
+string = "TextInRandomOrder"
+lst = set();
+lst.add("T");
+lst.add("I");
+lst.add("R");
+lst.add("O");
+print(lst);
+counter = 0;
+for char in string:
+    if char in lst:
+        print("Found");
+        counter += 1;
+print(counter);
