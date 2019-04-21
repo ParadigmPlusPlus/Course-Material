77d608e9aca51b048e514830956634facd8d0a46
Lemonsity <youzhang.mark.sun@gmail.com>
2019-04-21 19:59:01 -0400

    Class for method

--- Lesson-6/MethodNFunction.py
+++ Lesson-6/MethodNFunction.py
@@ -1,8 +1,15 @@
-def m(message):
-    print(message)
-    message += " Test"
-    return
+import random
+def findGD(a):
+    great = 0
+    prev = a[0]
+    for v in a:
+        great = max(great, abs(prev - v))
+        prev = v
+    return great
 
-v = "W"
-m(v)
-print(v)
+
+a = [random.randint(1, 100) for i in range (10)]
+print(*a, sep = ", ")
+print("Greatest difference between 2 adjacent element in an unsorted list is: %d" % findGD(a)) # first time using the method
+a.sort()
+print("Greatest difference between 2 adjacent element in a sorted list is: %d" % findGD(a)) # second time using the method
