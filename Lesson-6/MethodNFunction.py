e6cc126ef03f3d5cce35c4cc7354790e11389fa7
Lemonsity <youzhang.mark.sun@gmail.com>
2019-04-21 20:32:54 -0400

    Update

--- Lesson-6/MethodNFunction.py
+++ Lesson-6/MethodNFunction.py
@@ -12,4 +12,5 @@ a = [random.randint(1, 100) for i in range (10)]
 print(*a, sep = ", ")
 print("Greatest difference between 2 adjacent element in an unsorted list is: %d" % findGD(a)) # first time using the method
 a.sort()
+print(*a, sep = ", ")
 print("Greatest difference between 2 adjacent element in a sorted list is: %d" % findGD(a)) # second time using the method
